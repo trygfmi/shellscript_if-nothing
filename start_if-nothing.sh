5b97b27a10460341b6d0772ceadbb6f4a543fdc6
terner-running-terminal-commands <runningterminalcommands@gmail.com>
2026-01-07 13:59:34 +0900

    first main commit

---
+++ start_if-nothing.sh
@@ -0,0 +1,13 @@
+# ./start_if-nothing.sh
+
+
+i=0
+if [ $i == 0 ]; then
+    # echo "execute if"
+elif [ $i == 1 ]; then
+    # echo "execute elif"
+    :
+else
+    # echo "execute else"
+    :
+fi
